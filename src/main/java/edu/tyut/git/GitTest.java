1f6e578ca78ea64fb39d672b4ead4283a7d4396f
zhangshuhao <2905154639@qq.com>
2022-03-26 23:27:50 +0800

    master commit3 Changes

--- src/main/java/edu/tyut/git/GitTest.java
+++ src/main/java/edu/tyut/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2!");
-        System.out.println("Hello Git hot-fix!");
+        System.out.println("Hello Git master!");
     }
 }
